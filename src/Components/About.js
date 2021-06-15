b1ae9460eee89117e02f2e392464902c1c5bd941
moukim hfaidh <moukimhfaid@gmail.com>
2021-06-15 17:59:37 +0200

    some projects added

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,7 +13,6 @@ class About extends Component {
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
@@ -40,7 +39,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a onClick={ () => window.open('./pdf/Moukim_hfaiedh.pdf', '_none')}  className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
